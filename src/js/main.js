5f16b66b9a817fd7743007cf4b2c811e14548c86
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-05-04 12:13:49 +0200

    Updated SVG-icons styling

--- src/js/main.js
+++ src/js/main.js
@@ -101,9 +101,9 @@ document.addEventListener("DOMContentLoaded", () => {
     "#email-button-placeholder"
   ).innerHTML = `<a class="button is-rounded is-secondary" href="mailto:patrick.obermeier@outlook.com"
     referrerpolicy="no-referrer" rel="noreferrer noopener" target="_blank" id="mail-button">
-    <span class="icon small">
-      <svg width="25" height="20" viewBox="0 0 32 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M28.8 0H3.2C1.44 0 0.016 1.40625 0.016 3.125L0 21.875C0 23.5938 1.44 25 3.2 25H28.8C30.56 25 32 23.5938 32 21.875V3.125C32 1.40625 30.56 0 28.8 0ZM28.8 6.25L16 14.0625L3.2 6.25V3.125L16 10.9375L28.8 3.125V6.25Z" fill="#4A4A4A"/>
+    <span class="icon is-small">
+      <svg width="32" height="25" viewBox="0 0 32 25" fill="#4A4A4A" xmlns="http://www.w3.org/2000/svg">
+      <path d="M28.8 0H3.2C1.44 0 0.016 1.40625 0.016 3.125L0 21.875C0 23.5938 1.44 25 3.2 25H28.8C30.56 25 32 23.5938 32 21.875V3.125C32 1.40625 30.56 0 28.8 0ZM28.8 6.25L16 14.0625L3.2 6.25V3.125L16 10.9375L28.8 3.125V6.25Z"/>
       </svg>    
     </span>
     <span>Email</span>
@@ -113,8 +113,8 @@ document.addEventListener("DOMContentLoaded", () => {
   link.classList.add("navbar-item", "is-hidden-touch");
   link.id = "mail-icon";
   link.href = "mailto:patrick.obermeier@outlook.com";
-  link.innerHTML = `<span class="icon small">
-                      <svg width="25" height="20" viewBox="0 0 32 25" fill="#4A4A4A" xmlns="http://www.w3.org/2000/svg">
+  link.innerHTML = `<span class="icon is-small">
+                      <svg width="32" height="25" viewBox="0 0 32 25" fill="#4A4A4A" xmlns="http://www.w3.org/2000/svg">
                       <path d="M28.8 0H3.2C1.44 0 0.016 1.40625 0.016 3.125L0 21.875C0 23.5938 1.44 25 3.2 25H28.8C30.56 25 32 23.5938 32 21.875V3.125C32 1.40625 30.56 0 28.8 0ZM28.8 6.25L16 14.0625L3.2 6.25V3.125L16 10.9375L28.8 3.125V6.25Z"/>
                       </svg>
                     <span class="sr-only">Email</span>
