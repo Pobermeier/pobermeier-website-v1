57afcedc67bc502fc2ad7ab25b02023aacc95ee2
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-05-06 12:53:33 +0200

    Added social media icons to footer on all pages

--- src/js/main.js
+++ src/js/main.js
@@ -124,18 +124,35 @@ document.addEventListener("DOMContentLoaded", () => {
   </a>`;
   }
 
-  var link = document.createElement("a");
-  link.classList.add("navbar-item", "is-hidden-touch");
-  link.id = "mail-icon";
-  link.href = "mailto:patrick.obermeier@outlook.com";
-  link.innerHTML = `<span class="icon is-small">
+  const emailIcon = document.createElement("a");
+  emailIcon.classList.add("navbar-item", "is-hidden-touch");
+  emailIcon.id = "mail-icon";
+  emailIcon.href = "mailto:patrick.obermeier@outlook.com";
+  emailIcon.innerHTML = `<span class="icon is-small">
                       <svg width="32" height="25" viewBox="0 0 32 25" fill="#4A4A4A" xmlns="http://www.w3.org/2000/svg">
                       <path d="M28.8 0H3.2C1.44 0 0.016 1.40625 0.016 3.125L0 21.875C0 23.5938 1.44 25 3.2 25H28.8C30.56 25 32 23.5938 32 21.875V3.125C32 1.40625 30.56 0 28.8 0ZM28.8 6.25L16 14.0625L3.2 6.25V3.125L16 10.9375L28.8 3.125V6.25Z"/>
                       </svg>
                     <span class="sr-only">Email</span>
                     </span>`;
-  const emailIcon = document.querySelector("#email-icon-placeholder");
-  emailIcon && emailIcon.replaceWith(link);
+  const emailIconPlaceholder = document.querySelector(
+    "#email-icon-placeholder"
+  );
+  emailIconPlaceholder && emailIconPlaceholder.replaceWith(emailIcon);
+  const emailIconFooterPlaceholder = document.querySelector(
+    "#email-icon-placeholder-footer"
+  );
+  const emailIconFooter = emailIcon.cloneNode(true);
+  console.log(emailIconFooter);
+  console.log(emailIconFooterPlaceholder);
+  emailIconFooter.classList.remove("navbar-item", "is-hidden-touch");
+  emailIconFooter.innerHTML = `<span class="icon">
+  <svg width="32" height="25" viewBox="0 0 32 25" fill="#4A4A4A" xmlns="http://www.w3.org/2000/svg">
+  <path d="M28.8 0H3.2C1.44 0 0.016 1.40625 0.016 3.125L0 21.875C0 23.5938 1.44 25 3.2 25H28.8C30.56 25 32 23.5938 32 21.875V3.125C32 1.40625 30.56 0 28.8 0ZM28.8 6.25L16 14.0625L3.2 6.25V3.125L16 10.9375L28.8 3.125V6.25Z"/>
+  </svg>
+<span class="sr-only">Email</span>
+</span>`;
+  emailIconFooterPlaceholder &&
+    emailIconFooterPlaceholder.replaceWith(emailIconFooter);
 
   // Fix for 100vh height of hero-section on Mobile
   convertWindowHeightToViewPortHeight();
@@ -158,7 +175,7 @@ function fetchStyle(url) {
 }
 
 function lazyLoadBackgroundImages() {
-  var lazyBackgrounds = [].slice.call(
+  const lazyBackgrounds = [].slice.call(
     document.querySelectorAll(".lazy-background")
   );
 
