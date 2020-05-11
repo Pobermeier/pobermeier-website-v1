2b9156ec932cf41f1d43f03e817ec88abf8ca7d6
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-05-11 21:02:53 +0200

    Updated imagemin-config

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
-  mozjpeg: { progressive: true, quality: 60 },
+  mozjpeg: { progressive: true, quality: 75 },
   pngquant: { quality: [0.25, 0.5] },
   svgo: {
     plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
   },
-  webp: { quality: 60 },
+  webp: { quality: 75 },
 };
